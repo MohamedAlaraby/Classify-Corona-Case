0648642555265ebef6f4230bc3ca9d8632185ab0
Mohamed Elaraby <mohamedelaraby1002@gmail.com>
2022-05-19 15:14:07 +0200

    SW project

--- app/src/main/java/com/example/tensorflowlitedemo/IdentityActivity.java
+++ app/src/main/java/com/example/tensorflowlitedemo/IdentityActivity.java
@@ -10,32 +10,23 @@ import android.widget.Button;
 import com.google.android.material.button.MaterialButton;
 
 public class IdentityActivity extends AppCompatActivity {
-MaterialButton btn_old_patient,btn_new_patient,btn_admin;
+MaterialButton btn_patient,btn_admin;
     Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identity);
-        btn_old_patient=findViewById(R.id.Ident_Activity_btn_old_Patient);
+        btn_patient=findViewById(R.id.Ident_Activity_btn_old_Patient);
         btn_admin=findViewById(R.id.Ident_Activity_btn_admin);
-       btn_new_patient=findViewById(R.id.Ident_Activity_btn_new_Patient);
-        btn_old_patient.setOnClickListener(new View.OnClickListener() {
+
+        btn_patient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 intent=new Intent(IdentityActivity.this,LoginScreen.class);
                 startActivity(intent);
             }
         });
-        btn_new_patient.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                intent=new Intent(IdentityActivity.this,PatientInfoActivity.class);
-                startActivity(intent);
 
-
-
-            }
-        });
         btn_admin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
